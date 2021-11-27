0cc46f3a7a674cb78b5fcfbafb149a9241db67f9
Nikolay <nchernov@gmail.com>
2021-11-27 12:08:54 +0200

    work

--- DicomAnonymizer/DicomAnonymizer.ts
+++ DicomAnonymizer/DicomAnonymizer.ts
@@ -1,4 +1,5 @@
 import { DataSet, parseDicom } from 'dicom-parser';
+import { dataDictionary } from './dataDictionary';
 
 export default class DicomAnonymizer {
   constructor(private originBuffer: Uint8Array) {}
@@ -6,10 +7,27 @@ export default class DicomAnonymizer {
   anonymize = (): Uint8Array => {
     const dicom: DataSet = parseDicom(this.originBuffer);
     for (const element in dicom.elements) {
-      const v = dicom.string(element);
       const { tag, vr, length, dataOffset } = dicom.elements[element];
+      const description = dataDictionary[tag];
+      let name = '???';
+      let vm = '???';
+      if (description) {
+        name = description.name;
+        vm = description.vm;
+      } else {
+        console.warn(`[${vr}] Unknown tag:[${tag}]`);
+      }
 
-      console.log(v);
+      switch (vr) {
+        case 'PN': {
+          let value = '';
+          if (length > 0) value = dicom.string(element);
+          console.log(`[${vr}] ${name}:[${value}]`);
+        }
+        default: {
+          console.warn(`Skip vr:[${vr}]`);
+        }
+      }
     }
     return dicom.byteArray;
   };
