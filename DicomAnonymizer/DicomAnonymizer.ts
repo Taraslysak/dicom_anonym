0b39cf87b4ff30c00feaa73b1ccd898daab4fe6c
Nikolay <nchernov@gmail.com>
2021-11-27 11:21:51 +0200

    work

--- DicomAnonymizer/DicomAnonymizer.ts
+++ DicomAnonymizer/DicomAnonymizer.ts
@@ -5,6 +5,12 @@ export default class DicomAnonymizer {
 
   anonymize = (): Uint8Array => {
     const dicom: DataSet = parseDicom(this.originBuffer);
+    for (const element in dicom.elements) {
+      const v = dicom.string(element);
+      const { tag, vr, length, dataOffset } = dicom.elements[element];
+
+      console.log(v);
+    }
     return dicom.byteArray;
   };
 }
