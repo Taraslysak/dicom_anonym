1fbfced9e717492a8f414d358e7e69a68f541081
Nikolay <nchernov@gmail.com>
2021-11-28 10:49:26 +0200

    handler skeleton

--- DicomAnonymizer/DicomAnonymizer.ts
+++ DicomAnonymizer/DicomAnonymizer.ts
@@ -1,5 +1,6 @@
 import { DataSet, parseDicom } from 'dicom-parser';
 import { dataDictionary } from './dataDictionary';
+import { listAnonymizedTags } from './listAnonymizedTags';
 
 export default class DicomAnonymizer {
   constructor(private originBuffer: Uint8Array) {}
@@ -17,18 +18,45 @@ export default class DicomAnonymizer {
       } else {
         console.warn(`[${vr}] Unknown tag:[${tag}]`);
       }
+      if (!(tag in listAnonymizedTags)) {
+        console.log(`...skip ${tag}:${vr} [${name}]`);
+        continue;
+      }
 
-      switch (vr) {
-        case 'PN': {
-          let value = '';
-          if (length > 0) value = dicom.string(element);
-          console.log(`[${vr}] ${name}:[${value}]`);
-        }
-        default: {
-          console.warn(`Skip vr:[${vr}]`);
-        }
+      console.log(`Name: [${name}]`);
+
+      if (vr in handler) {
+        handler[vr](dicom, dataOffset, length);
+      } else {
+        console.warn(`Unknown vr:[${vr}]`);
       }
     }
     return dicom.byteArray;
   };
 }
+
+const loadString8 = (
+  byteArr: Uint8Array,
+  offset: number,
+  length: number
+): string => {
+  if (length <= 0) {
+    return '';
+  }
+  const bytes = byteArr.slice(offset, offset + length);
+  let retVal: string = '';
+  for (const c of bytes) {
+    retVal += String.fromCharCode(c);
+  }
+  return retVal;
+};
+
+const handler: {
+  [vr: string]: (dicom: DataSet, offset: number, length?: number) => void;
+} = {
+  PN: (dicom: DataSet, offset: number, length?: number) => {
+    const value = loadString8(dicom.byteArray, offset, length);
+    // if (length > 0) value = dicom.string(element);
+    console.log(`Origin Value: [PN][${value}]`);
+  },
+};
