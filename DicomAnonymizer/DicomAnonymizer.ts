a26b782dddbedebb61d7851394d4d15a5413f2c5
Nikolay <nchernov@gmail.com>
2021-11-28 15:01:05 +0200

    done

--- DicomAnonymizer/DicomAnonymizer.ts
+++ DicomAnonymizer/DicomAnonymizer.ts
@@ -1,11 +1,21 @@
-import { DataSet, parseDicom } from "dicom-parser";
-import { dataDictionary } from "./dataDictionary";
-import { listAnonymizedTags } from "./listAnonymizedTags";
+import { DataSet, parseDicom } from 'dicom-parser';
+import { dataDictionary } from './dataDictionary';
+import { listAnonymizedTags } from './listAnonymizedTags';
 
 const charSet8: Uint8Array = new Uint8Array(
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-    .split("")
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 '
+    .split('')
     .map((char) => char.charCodeAt(0))
+);
+
+const charUpperSet8: Uint8Array = new Uint8Array(
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 '
+    .split('')
+    .map((char) => char.charCodeAt(0))
+);
+
+const charNumberSet8: Uint8Array = new Uint8Array(
+  '0123456789'.split('').map((char) => char.charCodeAt(0))
 );
 
 export default class DicomAnonymizer {
@@ -15,12 +25,13 @@ export default class DicomAnonymizer {
   }
 
   anonymize = (): Uint8Array => {
+    let vrs = new Set<string>();
     const dicom: DataSet = parseDicom(this.originBuffer);
     for (const element in dicom.elements) {
       const { tag, vr, length, dataOffset } = dicom.elements[element];
       const description = dataDictionary[tag];
-      let name = "???";
-      let vm = "???";
+      let name = '???';
+      let vm = '???';
       if (description) {
         name = description.name;
         vm = description.vm;
@@ -32,7 +43,13 @@ export default class DicomAnonymizer {
         continue;
       }
 
+      vrs.add(vr);
+
       console.log(`Name: [${name}]`);
+
+      if (['SH', 'ST'].includes(vr)) {
+        console.log(length);
+      }
 
       if (vr in handler && length) {
         handler[vr](dicom, dataOffset, length);
@@ -51,10 +68,10 @@ const loadString8 = (
   length: number
 ): string => {
   if (length <= 0) {
-    return "";
+    return '';
   }
   const bytes = byteArr.slice(offset, offset + length);
-  let retVal: string = "";
+  let retVal: string = '';
   for (const c of bytes) {
     retVal += String.fromCharCode(c);
   }
@@ -64,13 +81,30 @@ const loadString8 = (
 const getRandomUint8CharArray = (length: number): number[] => {
   let randomValue: number[] = [];
   for (const _ of new Array(length)) {
-    randomValue.push(getRandomChar());
+    randomValue.push(getRandomChar8());
   }
   return randomValue;
 };
 
-const getRandomChar = (): number => {
+const getRandomChar8 = (): number => {
   return charSet8[Math.floor(Math.random() * charSet8.length)];
+};
+
+const getRandomUpperChar8 = (): number => {
+  return charUpperSet8[Math.floor(Math.random() * charUpperSet8.length)];
+};
+
+const getRandomNumberChar8 = (): number => {
+  return charNumberSet8[Math.floor(Math.random() * charNumberSet8.length)];
+};
+
+const randomDate = (
+  start: Date = new Date(0),
+  end: Date = new Date()
+): Date => {
+  return new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  );
 };
 
 const handler: {
@@ -78,22 +112,76 @@ const handler: {
 } = {
   PN: (dicom: DataSet, offset: number, length: number = 0) => {
     for (let position = 0; position < length; position++) {
-      dicom.byteArray[offset + position] = getRandomChar();
+      dicom.byteArray[offset + position] = getRandomChar8();
     }
-  },
-  IS: (dicom: DataSet, offset: number, length: number = 0) => {
-    const value = loadString8(dicom.byteArray, offset, length);
-    console.log(value);
-  },
-  UL: (dicom: DataSet, offset: number, length: number = 0) => {
-    const value = loadString8(dicom.byteArray, offset, length);
-    console.log(value);
   },
   LO: (dicom: DataSet, offset: number, length: number = 0) => {
     const value = loadString8(dicom.byteArray, offset, length);
     console.log(value);
     for (let position = 0; position < length; position++) {
-      dicom.byteArray[offset + position] = getRandomChar();
+      dicom.byteArray[offset + position] = getRandomChar8();
+    }
+  },
+  CS: (dicom: DataSet, offset: number, length: number = 0) => {
+    const value = loadString8(dicom.byteArray, offset, length);
+    console.log(value);
+    for (let position = 0; position < length; position++) {
+      dicom.byteArray[offset + position] = getRandomUpperChar8();
+    }
+  },
+  UI: (dicom: DataSet, offset: number, length: number = 0) => {
+    const bytes = dicom.byteArray;
+    const value = loadString8(bytes, offset, length);
+    for (let position = 0; position < length; position++) {
+      const char: number = bytes[position + offset];
+      if (char === 0 || String.fromCharCode(char) === '.') continue;
+      bytes[offset + position] = getRandomNumberChar8();
+    }
+    console.log(value);
+  },
+  DA: (dicom: DataSet, offset: number, length: number = 0) => {
+    const value = loadString8(dicom.byteArray, offset, length);
+    const d = randomDate();
+    const dateStr = `${d.getFullYear()}${d
+      .getMonth()
+      .toString()
+      .padStart(2, '0')}${d.getDate().toString().padStart(2, '0')}`;
+    const bytes = dicom.byteArray;
+    const size = Math.min(length, dateStr.length);
+    for (let i = 0; i < size; i++) {
+      bytes[offset + i] = dateStr.charCodeAt(i);
+    }
+    // console.log(value);
+  },
+  TM: (dicom: DataSet, offset: number, length: number = 0) => {
+    const value = loadString8(dicom.byteArray, offset, length);
+    const d = randomDate();
+    const timeStr = `${d.getHours().toString().padStart(2, '0')}${d
+      .getMinutes()
+      .toString()
+      .padStart(2, '0')}${d.getSeconds().toString().padStart(2, '0')}.${d
+      .getMilliseconds()
+      .toString()
+      .padStart(6, '0')}`;
+    const bytes = dicom.byteArray;
+    const size = Math.min(length, timeStr.length);
+    for (let i = 0; i < size; i++) {
+      bytes[offset + i] = timeStr.charCodeAt(i);
+    }
+    console.log(value);
+  },
+  SH: (dicom: DataSet, offset: number, length: number = 0) => {
+    // TODO: need verify!
+    // const value = loadString8(dicom.byteArray, offset, length);
+    for (let position = 0; position < length; position++) {
+      dicom.byteArray[offset + position] = getRandomChar8();
+    }
+  },
+  ST: (dicom: DataSet, offset: number, length: number = 0) => {
+    // TODO: need verify!
+    // const value = loadString8(dicom.byteArray, offset, length);
+    for (let position = 0; position < length; position++) {
+      dicom.byteArray[offset + position] = getRandomChar8();
     }
   },
 };
--- __tests__/DicomAnonymizer.ts
+++ __tests__/DicomAnonymizer.ts
@@ -1,21 +1,40 @@
-import fs from "fs";
-import JSZip, { file } from "jszip";
-import dicomParser from "dicom-parser";
-import DicomAnonymizer from "../DicomAnonymizer/DicomAnonymizer";
-describe("Dicom Anonymizer", () => {
-  test("should be able to anonymize Dicom file", async () => {
+import fs from 'fs';
+import JSZip, { file } from 'jszip';
+import dicomParser from 'dicom-parser';
+import DicomAnonymizer from '../DicomAnonymizer/DicomAnonymizer';
+import { listAnonymizedTags } from '../DicomAnonymizer/listAnonymizedTags';
+describe('Dicom Anonymizer', () => {
+  test('should be able to anonymize Dicom file', async () => {
     const buffer = await prepareUint8Array();
     const initialDicom = dicomParser.parseDicom(buffer);
     const anonymizer = new DicomAnonymizer(buffer);
     const newBuffer = anonymizer.anonymize();
     const anonymizedDicom = dicomParser.parseDicom(newBuffer);
     expect(anonymizedDicom).toBeTruthy();
-    expect(anonymizedDicom.string("x00100010")).not.toBe(
-      initialDicom.string("x00100010")
+    expect(anonymizedDicom.string('x00100010')).not.toBe(
+      initialDicom.string('x00100010')
     );
-    expect(anonymizedDicom.string("x00100020")).not.toBe(
-      initialDicom.string("x00100020")
+    expect(anonymizedDicom.string('x00100020')).not.toBe(
+      initialDicom.string('x00100020')
     );
+    for (const element in initialDicom.elements) {
+      if (element in listAnonymizedTags) {
+        const initDesc = initialDicom.elements[element];
+        if (initDesc.length > 0) {
+          const anonDesc = anonymizedDicom.elements[element];
+          expect(initDesc).toEqual(anonDesc);
+          const initBytes = initialDicom.byteArray.slice(
+            initDesc.dataOffset,
+            initDesc.dataOffset + initDesc.length
+          );
+          const anonBytes = anonymizedDicom.byteArray.slice(
+            anonDesc.dataOffset,
+            anonDesc.dataOffset + anonDesc.length
+          );
+          expect(initBytes).not.toEqual(anonBytes);
+        }
+      }
+    }
   });
 });
 
@@ -29,5 +48,5 @@ const prepareUint8Array = async (): Promise<Uint8Array> => {
     optimizedBinaryString: true,
   });
 
-  return await Object.values(zipBuf.files)[0].async("uint8array");
+  return await Object.values(zipBuf.files)[0].async('uint8array');
 };
